e7a8a1ef0d2ec5f0754ba3c674db9fdfc088b804
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-10-27 22:22:08 +0530

    create map component and render the map  using react-native-maps

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,26 +1,41 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import MapView from "react-native-maps";
+import React, { useState } from "react";
+import MapView, { Marker } from "react-native-maps";
 import { styled } from "nativewind";
 import { useSelector } from "react-redux";
 import { selectOrigin } from "../slices/navSlice";
 
-const StyledMapview = styled(MapView);
+const StyledMapView = styled(MapView);
 
 const MapComponent = () => {
   const origin = useSelector(selectOrigin);
 
+  const [initialRegion, setInitialRegion] = useState({
+    latitude: origin.location.lat,
+    longitude: origin.location.lng,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  });
+
   return (
-    <StyledMapview
+    <StyledMapView
       className="flex-1"
       mapType="mutedStandard"
-      initialRegion={{
-        latitude: origin.location.lat,
-        longitude: origin.location.lng,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-      }}
-    />
+      initialRegion={initialRegion}
+      onRegionChangeComplete={(region) => setInitialRegion(region)}
+    >
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+    </StyledMapView>
   );
 };
 
