e0b61e1575229aec001715e758d8f2446c082cc2
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-10-30 12:50:37 +0530

    add tailwind config

--- App.js
+++ App.js
@@ -1,42 +1,48 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View, KeyboardAvoidingView, Platform } from "react-native";
 import { Provider } from "react-redux";
-import { store } from "./store";
-import HomeScreen from "./screens/HomeScreen";
-import { withExpoSnack } from "nativewind";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TailwindProvider } from "tailwindcss-react-native";
+import HomeScreen from "./screens/HomeScreen";
+import { store } from "./store";
 import MapScreen from "./screens/MapScreen";
-import { KeyboardAvoidingView, Platform } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
-function App() {
+export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
-          >
-            <Stack.Navigator initialRouteName="HomeScreen">
-              <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="MapScreen"
-                component={MapScreen}
-                options={{ headerShown: false }}
-              />
-            </Stack.Navigator>
-          </KeyboardAvoidingView>
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <TailwindProvider>
+            <KeyboardAvoidingView
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
+              style={{ flex: 1 }}
+              keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            >
+              <Stack.Navigator>
+                <Stack.Screen
+                  name="HomeScreen"
+                  component={HomeScreen}
+                  options={{
+                    headerShown: false,
+                  }}
+                />
+                <Stack.Screen
+                  name="MapScreen"
+                  component={MapScreen}
+                  options={{
+                    headerShown: false,
+                  }}
+                />
+              </Stack.Navigator>
+            </KeyboardAvoidingView>
+          </TailwindProvider>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
-
-export default withExpoSnack(App);
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        "./App.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./screens/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
